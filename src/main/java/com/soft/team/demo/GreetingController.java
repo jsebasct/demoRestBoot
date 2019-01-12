15778e3843c56c8ceb63bef66062fd327ac6b656
jscruz <jsebas.ct@gmail.com>
2019-01-11 23:01:24 -0300

    feat: post without parameters

--- src/main/java/com/soft/team/demo/GreetingController.java
+++ src/main/java/com/soft/team/demo/GreetingController.java
@@ -1,6 +1,7 @@
 package com.soft.team.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,19 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value="/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    @RequestMapping(value="/greeting", method = RequestMethod.POST)
+    public Greeting greetingPost() {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, "Vacio"));
+    }
+
+
+
 
 }
